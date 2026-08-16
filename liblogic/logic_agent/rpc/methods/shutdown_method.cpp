62d81de7105968c6c6a21984753a456e634a0ddc
thonkdifferent <41342923+thonkdifferent@users.noreply.github.com>
2026-08-16 13:48:26 +0300

    Shutdown method; usage docs

---
+++ liblogic/logic_agent/rpc/methods/shutdown_method.cpp
@@ -0,0 +1,11 @@
+#include "shutdown_method.hpp"
+
+namespace logic_agent::rpc::methods
+{
+    nlohmann::json handle_shutdown(const nlohmann::json& /*params*/, event_sink& /*sink*/)
+    {
+        nlohmann::json result;
+        result["message"] = "shutting down";
+        return result;
+    }
+}
